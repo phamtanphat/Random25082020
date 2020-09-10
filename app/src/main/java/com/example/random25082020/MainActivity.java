6b07d5838302611b19555170335032fceb0f8e5b
phatdroid94 <phatdroid94@gmail.com>
2020-09-10 21:32:05 +0700

    xu ly mang

--- app/src/main/java/com/example/random25082020/MainActivity.java
+++ app/src/main/java/com/example/random25082020/MainActivity.java
@@ -11,14 +11,18 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
+import java.util.Vector;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText mEdtSoMin, mEdtSoMax;
     Button mBtnRandom;
     TextView mTvKetQua;
-
+    String mKetQua = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,12 +36,23 @@ public class MainActivity extends AppCompatActivity {
         //ctrl + p : xem tham số truyền vào
         // Annotation : chú thích
 
+        List<Integer> arrNums = new ArrayList<>();
+        // Them dữ liệu
+        arrNums.add(1); //index = 0
+        arrNums.add(2); //index = 1
+        arrNums.add(3); //index = 2
+        // Xóa dữ liệu
+        arrNums.remove(0);
+        // Cập nhật dữ liệu
+        arrNums.set(0 , 5);
+
         mBtnRandom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//              Lấy dữ liệu từ edittext
                 String textSoMin = mEdtSoMin.getText().toString();
                 String textSoMax = mEdtSoMax.getText().toString();
-
+//              Kiểm tra chuỗi rỗng
                 if (textSoMin.isEmpty() || textSoMax.isEmpty()) {
                     Toast.makeText(
                             MainActivity.this,
@@ -46,11 +61,23 @@ public class MainActivity extends AppCompatActivity {
                     ).show();
                     return;
                 }
-
+//              Chuyển dữ liệu thành số
                 int sMin = Integer.parseInt(textSoMin);
                 int sMax = Integer.parseInt(textSoMax);
 
+//              Kiểm tra số min không được hơn hoặc bằng số max
+                if (sMin >= sMax){
+                    sMax = sMin + 1;
 
+                }
+                mEdtSoMax.setText(String.valueOf(sMax));
+                mEdtSoMin.setText(String.valueOf(sMin));
+
+                // random
+                Random random = new Random();
+                int value = random.nextInt(sMax - sMin + 1) + sMin;
+                mKetQua += value + " - ";
+                mTvKetQua.setText(mKetQua);
             }
         });
     }
