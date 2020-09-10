50d9e42b2c9ea3e3cc4931fc265686af684b940b
phatdroid94 <phatdroid94@gmail.com>
2020-09-10 20:56:04 +0700

    lay du lieu tu edittext

--- app/src/main/java/com/example/random25082020/MainActivity.java
+++ app/src/main/java/com/example/random25082020/MainActivity.java
@@ -3,36 +3,55 @@ package com.example.random25082020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText mEdtSoMin, mEdtSoMax;
+    Button mBtnRandom;
+    TextView mTvKetQua;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Ánh xạ
+        mEdtSoMax = findViewById(R.id.edtSomax);
+        mEdtSoMin = findViewById(R.id.edtSomin);
+        mBtnRandom = findViewById(R.id.btnRandom);
+        mTvKetQua = findViewById(R.id.tvKetqua);
 
-//        float number = 1.5f;
-//        Lam tron theo 0.5
-//        int result = Math.round(number);
-//        Làm tròn lên
-//        double number = 1.01;
-//        double result = Math.ceil(number);
-//        Làm tròn xuống
-//        double number = 1.999;
-//        double result = Math.floor(number);
-        // 5 -> 10
-//        double result = Math.floor(Math.random() * 6) + 5;
-//        Log.d("BBB",String.valueOf(result));
-//        Random random = new Random();
-//        for (int i = 0; i <100 ; i++) {
-//            int number = random.nextInt(somax - smin + 1) + smin;
-//            Log.d("BBB",String.valueOf(number));
-//        }
+        //ctrl + p : xem tham số truyền vào
+        // Annotation : chú thích
+
+        mBtnRandom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textSoMin = mEdtSoMin.getText().toString();
+                String textSoMax = mEdtSoMax.getText().toString();
+
+                if (textSoMin.isEmpty() || textSoMax.isEmpty()) {
+                    Toast.makeText(
+                            MainActivity.this,
+                            "Bạn chưa nhập đủ thông tin",
+                            Toast.LENGTH_SHORT
+                    ).show();
+                    return;
+                }
+
+                int sMin = Integer.parseInt(textSoMin);
+                int sMax = Integer.parseInt(textSoMax);
 
 
-
+            }
+        });
     }
 }
